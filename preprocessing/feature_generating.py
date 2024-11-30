7eb4c4fae9470c641d9f0a192e6e4357ef8f3947
Halva773 <Selin0106@mail.ru>
2024-11-30 20:17:47 +0300

    Добавлены фичи на основе salary

--- preprocessing/feature_generating.py
+++ preprocessing/feature_generating.py
@@ -1,14 +1,11 @@
-import re
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import re
 import json
+import re
 from datetime import datetime
+
+import pandas as pd
 from dateutil.relativedelta import relativedelta
 
-from formating import split_work_experience as unique_work
+from utils import split_work_experience as unique_work
 
 
 def calculate_experience_months(data):
@@ -39,8 +36,10 @@ def calculate_experience_months(data):
             merged_intervals[-1] = (merged_intervals[-1][0], max(merged_intervals[-1][1], end))
 
     # Подсчёт суммарной продолжительности в месяцах
-    total_months = sum((relativedelta(end, start).years * 12 + relativedelta(end, start).months) for start, end in merged_intervals)
+    total_months = sum(
+        (relativedelta(end, start).years * 12 + relativedelta(end, start).months) for start, end in merged_intervals)
     return total_months
+
 
 def generate_worker_features(df):
     df['unique_work'] = df['work_experience'].apply(unique_work)
@@ -49,9 +48,67 @@ def generate_worker_features(df):
     df['avg_time_per_work'] = df['work_experience_months'] / df['count_works']
     return df
 
+
+def extract_salaries(df, salary_column):
+    """
+    Извлекает минимальную и комфортную зарплату из текстовой колонки, а также грейд.
+
+    Args:
+        df (pd.DataFrame): Исходный датафрейм.
+        salary_column (str): Имя текстовой колонки с информацией о зарплате.
+
+    Returns:
+        pd.DataFrame: Датафрейм с добавленными колонками `min_salary`, `comfort_salary` и `grade`.
+    """
+
+    def process_salary(salary_text):
+        if len(salary_text) < 2:
+            return 0, 0, 0, 0
+        # Проверка на наличие грейда
+        grade_match = re.search(r'(?:\bгрейд\s*\d+|\d+\s*-?[й-]?\s*грейд)', salary_text, flags=re.IGNORECASE)
+        if grade_match:
+            # Если найден грейд, извлекаем его и очищаем текст
+            grade = int(re.search(r'\d+', grade_match.group(0)).group())
+            # Удаляем текст с грейдом из salary
+            salary_text = re.sub(r'(?:\bгрейд\s*\d+|\d+\s*-?[й-]?\s*грейд)', '', salary_text, flags=re.IGNORECASE)
+            return 0, 0, grade, salary_text.strip()
+
+        matches = re.findall(r'\b\d{1,3}(?:[\.,]\d{1,3})*(?:\s\d{3})?\s?(?:евро)\b', salary_text)
+
+        numbers = [int(match.split(' ')[0].replace(" ", "").replace(".", "").replace(',', '')) for match in matches]
+        numbers = [i * 100 * 90 if i < 100 else i * 90 for i in numbers]
+        if numbers:
+            return min(numbers), max(numbers), 0, salary_text
+
+        # Находим числа из 2 до 6 цифр, поддерживая разделители пробелом или точкой
+        matches = re.findall(r'\b\d{2,6}(?:[\s.,]\d{3})?', salary_text)
+        numbers = [int(match.replace(" ", "").replace(".", "").replace(',', '')) for match in matches]
+
+        numbers = [i * 1000 if i < 1000 else i for i in numbers]
+        # Возвращаем минимальную и комфортную зарплату
+        if numbers:
+            return min(numbers), max(numbers), 0, salary_text
+        return 0, 0, 0, salary_text
+
+    # Применяем функцию к каждой строке в колонке salary_column
+    df[['min_salary', 'comfort_salary', 'grade', 'updated_salary']] = df[salary_column].apply(
+        lambda x: pd.Series(process_salary(x) if isinstance(x, str) else (0, 0, 0, x))
+    )
+    df.loc[(df.min_salary == 0) & (df.comfort_salary != 0), 'min_salary'] = df.loc[
+        (df.min_salary == 0) & (df.comfort_salary != 0), 'comfort_salary']
+    df.loc[(df.min_salary != 0) & (df.comfort_salary == 0), 'comfort_salary'] = df.loc[
+        (df.min_salary != 0) & (df.comfort_salary == 0), 'min_salary']
+    # Обновляем колонку salary с учетом удаленного текста про грейд
+    #     df[salary_column] = df['updated_salary']
+    df.drop(columns=['updated_salary'], inplace=True)
+
+    return df
+
+
 if __name__ == '__main__':
     with open('../data/client_dataset.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
 
     df = generate_worker_features(pd.DataFrame(data))
-    print(df[['unique_work', 'count_works', 'work_experience_months', 'avg_time_per_work']].head(10))
+    data = extract_salaries(data, 'salary')
+    print(df[['min_salary', 'comfort_salary', 'grade', 'unique_work', 'count_works', 'work_experience_months', 'avg_time_per_work']].head(10))
